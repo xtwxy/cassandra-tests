c060fbbabe6b11ffc162ee770a9646b6910243bb
Wangxy <xtwxy@hotmail.com>
2017-03-16 10:33:27 +0800

    delete unused.

--- test-ipmi/src/main/java/io/netty/example/udt/echo/message/MsgEchoClient.java
+++ test-ipmi/src/main/java/io/netty/example/udt/echo/message/MsgEchoClient.java
@@ -40,7 +40,7 @@ public final class MsgEchoClient {
 
     private static final Logger log = Logger.getLogger(MsgEchoClient.class.getName());
 
-    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final String HOST = System.getProperty("host", "192.168.0.69");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8007"));
     static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
