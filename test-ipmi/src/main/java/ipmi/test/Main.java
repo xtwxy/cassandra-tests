c20f9fa9da92b8427821498c0cc211cb78750f9f
Wangxy <Wangxy@WXY-SQLServer>
2017-03-16 10:41:22 +0800

    add launcher for echo client and server.

--- test-ipmi/src/main/java/ipmi/test/Main.java
+++ test-ipmi/src/main/java/ipmi/test/Main.java
@@ -1,8 +1,11 @@
 package ipmi.test;
 
+import io.netty.example.udt.echo.message.MsgEchoClient;
+import io.netty.example.udt.echo.message.MsgEchoServer;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		if (args.length >= 1) {
 			String clientOrServer = args[0];
 			if ("server".equalsIgnoreCase(clientOrServer)) {
@@ -16,6 +19,10 @@ public class Main {
 			} else if ("client".equalsIgnoreCase(clientOrServer)) {
 				Client client = new Client();
 				client.start();
+			} else if ("echo-client".equalsIgnoreCase(clientOrServer)) {
+				MsgEchoClient.main(args);
+			} else if ("echo-server".equalsIgnoreCase(clientOrServer)) {
+				MsgEchoServer.main(args);
 			} else {
 				usage();
 			}
